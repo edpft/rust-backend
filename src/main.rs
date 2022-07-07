702638cfcfe9a4dc6c06a8553e8eda1a2c7b7bb0
Ed Fawcett-Taylor <edwardpaultaylor@gmail.com>
2022-07-07 22:39:52 +0100

    add `add_client` routes

--- src/main.rs
+++ src/main.rs
@@ -5,9 +5,17 @@ mod routes;
 #[cfg(test)]
 mod tests;
 
-use routes::health_check;
+use rocket::fs::{FileServer, relative};
+
+use rocket_dyn_templates::Template;
+
+use routes::{health_check, index, submit};
 
 #[launch]
 fn rocket() -> _ {
-    rocket::build().mount("/", routes![health_check])
+    rocket::build()
+        .mount("/health_check", routes![health_check])
+        .mount("/add_client", routes![index, submit])
+        .attach(Template::fairing())
+        .mount("/add_client", FileServer::from(relative!("/static")))
 }
--- src/routes.rs
+++ src/routes.rs
@@ -1,6 +1,32 @@
+use rocket::form::{Context, Contextual, Form, FromForm};
 use rocket::http::Status;
+use rocket::post;
+use rocket_dyn_templates::Template;
 
-#[get("/health_check")]
+#[get("/")]
 pub async fn health_check() -> Status {
     Status::Ok
+}
+
+#[derive(Debug, FromForm)]
+pub struct Client<'v> {
+    #[field(validate = len(1..))]
+    pub first_name: &'v str,
+    #[field(validate = len(1..))]
+    pub last_name: &'v str,
+    #[field(validate = contains('@').or_else(msg!("invalid email address")))]
+    pub email_address: &'v str,
+    pub telephone_number: &'v str,
+}
+
+#[get("/")]
+pub async fn index() -> Template {
+    Template::render("index", &Context::default())
+}
+
+#[post("/", data = "<client>")]
+pub async fn submit<'r>(client: Form<Contextual<'r, Client<'r>>>) -> (Status, Template) {
+    let template = Template::render("index", &client.context);
+
+    (client.context.status(), template)
 }

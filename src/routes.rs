b5f069be4be298924e30613bbacad47de1a4f101
Ed Fawcett-Taylor <edwardpaultaylor@gmail.com>
2022-07-18 20:15:39 +0100

    update `routes.rs` to use `models.rs`

--- src/routes.rs
+++ src/routes.rs
@@ -1,36 +1,46 @@
-use rocket::form::{Context, Contextual, Form, FromForm};
+use rocket::form::{Context, Contextual, Form};
 use rocket::http::Status;
-use rocket::post;
+use rocket::response::Redirect;
+use rocket::response::status::NotFound;
+use rocket::{post, State};
 use rocket_dyn_templates::Template;
+use sqlx::postgres::PgPool;
+
+use crate::models::{NewClient, Client};
 
 #[get("/")]
 pub async fn health_check() -> Status {
     Status::Ok
 }
 
-#[derive(Debug, FromForm)]
-pub struct Client<'v> {
-    #[field(validate = len(1..).or_else(msg!("Enter the client's first name")))]
-    pub first_name: &'v str,
-    #[field(validate = len(1..).or_else(msg!("Enter the client's last name")))]
-    pub last_name: &'v str,
-    #[field(validate = contains('@').or_else(msg!("Enter a valid email address")))]
-    pub email_address: &'v str,
-    pub telephone_number: &'v str,
-}
-
 #[get("/")]
 pub async fn index() -> Template {
-    Template::render("index", &Context::default())
+    Template::render("add_client", &Context::default())
 }
 
 #[post("/", data = "<client>")]
-pub async fn submit<'r>(client: Form<Contextual<'r, Client<'r>>>) -> (Status, Template) {
-    let template = match client.context.status().code {
-        200u16 => {
-            Template::render("success", &client.context)
+pub async fn add_client<'r>(
+    pool: &'r State<PgPool>,
+    client: Form<Contextual<'r, NewClient<'r>>>,
+) -> Result<Redirect, Template> {
+    match &client.value {
+        Some(client) => {
+            let new_client_id = client.add(pool.inner()).await.expect("Add client to database");
+            Ok(Redirect::to(format!("/client/{}", new_client_id)))
         }
-        _ => Template::render("index", &client.context),
-    };
-    (client.context.status(), template)
+        None => {
+            let template = Template::render("add_client", &client.context);
+            Err(template)
+        }
+    }
+}
+
+#[get("/<id>")]
+pub async fn get_client<'r>(pool: &'r State<PgPool>, id: i64) -> Result<Template, NotFound<String>> {
+    let client = Client::get(pool.inner(), id).await;
+    
+    match client {
+        Ok(client) => Ok(Template::render("client",  client)),
+        Err(error) => Err(NotFound(error.to_string()))
+    }
 }

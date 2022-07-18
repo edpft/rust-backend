5f4ebfb927ac354303d6189a71b3d4dfe3caa875
Ed Fawcett-Taylor <edwardpaultaylor@gmail.com>
2022-07-18 20:16:03 +0100

    update main to use new routes and models

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,40 @@
 #[macro_use]
 extern crate rocket;
 
+mod db;
+mod models;
 mod routes;
 #[cfg(test)]
 mod tests;
 
+use std::env;
+use dotenv::dotenv;
+
 use rocket::fs::{relative, FileServer};
-
 use rocket_dyn_templates::Template;
+use sqlx::postgres::PgPoolOptions;
 
-use routes::{health_check, index, submit};
+use routes::{health_check, index, add_client, get_client};
 
-#[launch]
-fn rocket() -> _ {
-    rocket::build()
-        .mount("/health_check", routes![health_check])
-        .mount("/add_client", routes![index, submit])
-        .attach(Template::fairing())
-        .mount("/add_client", FileServer::from(relative!("/static")))
+#[rocket::main]
+async fn main() -> anyhow::Result<()> {
+    dotenv().ok();
+    let database_url = env::var("DATABASE_URL")?;
+    
+    let pool = PgPoolOptions::new()
+    .max_connections(5)
+    .connect(&database_url)
+    .await?;
+    
+    let _ = rocket::build()
+    .mount("/health_check", routes![health_check])
+    .mount("/client", routes![get_client])
+    .mount("/add_client", routes![index, add_client])
+    .mount("/add_client", FileServer::from(relative!("/static")))
+    .manage(pool)
+    .attach(Template::fairing())
+    .launch()
+    .await;
+
+    Ok(())
 }
